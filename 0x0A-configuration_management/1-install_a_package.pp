a84d9e4c1962afdcdf3e4be52e008ac38d10dc73
Eldorado16 <65019139+Eldorado16@users.noreply.github.com>
2023-07-16 20:45:07 -0800

    Update 1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,5 @@
-# Installs puppet-lint
+#install flask from pip3
 
-package { 'puppet-lint':
-  ensure   => '2.5.0',
-  provider => 'gem',
+exec {'flask':
+  command => 'pip3 install flask==2.1.0',
 }
